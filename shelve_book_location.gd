d04b54ca3c9adbbb3876df8a36ad4c7f9755ec47
Corbin Matamoros <corbinmatamoros@yahoo.com>
2025-01-29 20:17:31 -0600

    add detection zones around bookshelves

---
+++ shelve_book_location.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var shelve_book_location = find_child("Area3D")
+	shelve_book_location.set_monitoring(true)
+	shelve_book_location.body_entered.connect(_on_shelve_book_location_entered)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_shelve_book_location_entered():
+	print("You entered the block!")
